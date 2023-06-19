eab8970474735f30669dfaec8ecdcaf337adbf9a
Kevin Koster <koster.k2001@gmail.com>
2023-06-19 22:05:16 +0200

    [lumina] remove extension reloading

--- lumina/execute.py
+++ lumina/execute.py
@@ -1,31 +1,16 @@
 import discord
-from discord.ext import commands
 
-from extensions import load_all_extensions, reload_extensions
+from extensions import load_all_extensions
 from lumina import get_config
 
 bot = discord.Bot()
 
 
-@bot.slash_command(description="Reload all extensions. Will also load new extensions.")
-@commands.has_role("Lumineer")
-async def reload(ctx):
-    errors = reload_extensions(bot=bot)
-    if not errors:
-        await ctx.send_response(
-            ":white_check_mark: Successfully reloaded all extensions.", delete_after=3
-        )
-    else:
-        await ctx.send_response(
-            ":x: Some extensions couldn't be loaded, see logs for more details. :",
-            delete_after=3,
-        )
-
-
 @bot.event
 async def on_connect():
     try:
-        load_all_extensions(bot=bot)
+        await load_all_extensions(bot=bot)
+        await bot.sync_commands()
     except Exception as e:
         raise e
 
